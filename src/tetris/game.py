3588c770601156a56bb2b7f33b2c57832f467fb6
0x51018 <younhj1018@gmail.com>
2026-04-17 19:35:39 +0900

    Feat: created main loop

--- src/tetris/game.py
+++ src/tetris/game.py
@@ -1,6 +1,31 @@
 import pygame
 from board import Board
+from event import EventHandler
 
 class Game:
-    def __init__(self):
+    def __init__(self, BASE_DIR, width, height, fps=60):
+        pygame.init()
+        self.BASE_DIR = BASE_DIR
+        self.screen = pygame.display.set_mode((width,height))
+        self.clock = pygame.time.Clock()
+        self.frame_rate = fps
+        self.isGameRunning = True
+
         self.game_board = Board()
+        self.event_handler = EventHandler(self.BASE_DIR)
+
+        print("Game instance initialized.")
+
+    def run(self):
+        print("Game.run() starting")
+        while self.isGameRunning:
+            """
+            Real Loop Logic Part. begin
+            """
+            self.event_handler.check()
+            """
+            end
+            """
+            self.clock.tick(self.frame_rate)
+            pygame.display.flip()
+        pygame.quit()
